95b66eb2fa2716f3fdb9e31566bcc2b1577de7ce
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2017-04-09 09:38:29 -0400

    Removed chown.

--- image/etc/my_init.d/set-home-permissions.sh
+++ image/etc/my_init.d/set-home-permissions.sh
@@ -8,9 +8,3 @@ fi
 if [ "$HOST_GID" ]; then
     groupmod -g $HOST_GID $DOCKER_GROUP
 fi
-
-# This makes sure that all files in $DOCKER_HOME are accessible by the user. 
-# We exclude the folder ~/shared to reduce IO out to the host. Docker
-# for Mac, Docker for Windows and the UID/GID trick above should mean that file
-# permissions work seamlessly now.
-find $DOCKER_HOME -maxdepth 1 | sed "1d" | grep -v "$DOCKER_HOME/shared" | xargs chown -R $DOCKER_USER:$DOCKER_GROUP 3> /dev/null || true
