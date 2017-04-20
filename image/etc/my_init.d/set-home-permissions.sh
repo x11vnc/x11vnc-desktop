386f63905c3aed58715e09751f10d0358f01ac39
Xiangmin Jiao <xmjiao@gmail.com>
2017-04-20 06:53:40 -0400

    Suppress warning messages.

--- image/etc/my_init.d/set-home-permissions.sh
+++ image/etc/my_init.d/set-home-permissions.sh
@@ -12,7 +12,7 @@ fi
 # This makes sure that all directories in HOME are accessible by the user.
 # This helps avoiding issues wiht mounted volumes.
 source /etc/container_environment.sh
-find $DOCKER_HOME -type d -maxdepth 1 | sed "1d" | xargs chown $DOCKER_USER:$DOCKER_GROUP 2> /dev/null || true
+find $DOCKER_HOME -maxdepth 1 -type d | sed "1d" | xargs chown $DOCKER_USER:$DOCKER_GROUP 2> /dev/null || true
 
 # It is important for $HOME/.ssh to have correct ownership
 chown -R $DOCKER_USER $DOCKER_HOME/.ssh
