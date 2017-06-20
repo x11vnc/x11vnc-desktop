9d5e67f3f42002f85c37b83f36e870ad87bd9aea
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-20 19:37:13 -0400

    Updated startvnc.sh

--- image/usr/local/bin/startvnc.sh
+++ image/usr/local/bin/startvnc.sh
@@ -10,7 +10,7 @@
 RESOLUT="${RESOLUT:-1440x900}"
 SIZE=`echo $RESOLUT | sed -e "s/x/ /"`
 grep -s -q $RESOLUT $DOCKER_HOME/.config/xorg.conf && \
-sed -i -e "s/Virtual 1440 900/Virtual $SIZE/" $DOCKER_HOME/.config/xorg.conf
+perl -i -p -e "s/Virtual \d+ \d+/Virtual $SIZE/" $DOCKER_HOME/.config/xorg.conf
 
 Xorg -noreset -logfile $DOCKER_HOME/.log/Xorg.log -config $DOCKER_HOME/.config/xorg.conf :0 2> $DOCKER_HOME/.log/Xorg_err.log &
 sleep 0.1
