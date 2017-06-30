bb2594279e6369275b35891d635b870fd4c7215f
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-30 01:02:26 -0400

    Changed error handling

--- ubuntu_desktop.py
+++ ubuntu_desktop.py
@@ -366,7 +366,8 @@ if __name__ == "__main__":
                 if not subprocess.check_output(['docker', 'ps',
                                                 '-q', '-f',
                                                 'name=' + container]):
-                    raise subprocess.CalledProcessError
+                    sys.stderr.write('Docker container is no longer running\n')
+                    sys.exit(-1)
                 else:
                     time.sleep(1)
                     continue
