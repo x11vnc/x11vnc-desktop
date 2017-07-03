e907f0de94c4a7fadf7608d2108430ab06cf1993
Xiangmin Jiao <xmjiao@gmail.com>
2017-07-03 15:17:01 -0400

    Specify path of vnc.html

--- image/usr/local/bin/startvnc.sh
+++ image/usr/local/bin/startvnc.sh
@@ -28,7 +28,17 @@ eval `ssh-agent` > /dev/null
 
 /usr/bin/lxsession -s LXDE -e LXDE > $DOCKER_HOME/.log/lxsession.log 2>&1 &
 
-(COUNTER=0; while [ $COUNTER -lt 100 ]; do WIN="$(xdotool search --name Error)"; if [ -n "$WIN" ]; then xdotool key --window $WIN space; echo "Resolved error $WIN after $COUNTER iterations"; break; fi; sleep 0.1; let COUNTER=COUNTER+1; done) &
+(COUNTER=0; \
+ while [ $COUNTER -lt 100 ]; do \
+     WIN="$(xdotool search --name Error)"; \
+     if [ -n "$WIN" ]; then \
+         xdotool key --window $WIN space; \
+         echo "Resolved error $WIN after $COUNTER iterations"; \
+         break; \
+     fi; \
+     sleep 0.1; \
+     let COUNTER=COUNTER+1; \
+done) &
 
 # startup x11vnc with a new password
 export VNCPASS=`openssl rand -base64 6 | sed 's/\//-/'`
@@ -43,4 +53,4 @@ echo "Open your web browser with URL:"
 echo "    http://localhost:6080/vnc.html?resize=downscale&autoconnect=1&password=$VNCPASS"
 
 # startup novnc
-/usr/local/noVNC/utils/launch.sh --listen 6080 > $DOCKER_HOME/.log/novnc.log 2>&1
+/usr/local/noVNC/utils/launch.sh --web /usr/local/noVNC --listen 6080 > $DOCKER_HOME/.log/novnc.log 2>&1
