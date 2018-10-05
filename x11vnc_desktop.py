4a990b196e736a7c78ce18445b428f6b53f765be
Xiangmin Jiao <xmjiao@gmail.com>
2018-10-05 19:13:02 -0400

    removed image/home/.docker_path

--- x11vnc_desktop.py
+++ x11vnc_desktop.py
@@ -18,7 +18,7 @@ import os
 owner = "x11vnc"
 proj = os.path.basename(sys.argv[0]).split('_')[0]
 image = owner + "/desktop"
-tag = "18.04"
+tag = "17.10"
 projdir = "project"
 workdir = "project"
 
@@ -36,7 +36,7 @@ def parse_args(description):
                         default=image)
 
     parser.add_argument('-t', '--tag',
-                        help='Tag of the image. The default is 18.04. ' +
+                        help='Tag of the image. The default is 17.10. ' +
                         'If the image already has a tag, its tag prevails.',
                         default=tag)
 
