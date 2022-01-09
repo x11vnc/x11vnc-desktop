fa4e7bb65d0666e81ed146fbe180a71962b0af64
Mike Hummel <mh@mhus.de>
2022-01-09 10:12:53 +0100

    fix lost environment and long startup

--- image/etc/my_init.d/init-home.sh
+++ image/etc/my_init.d/init-home.sh
@@ -14,11 +14,12 @@ fi
 if [ -e '/etc/container_environment.sh' ]; then
     source /etc/container_environment.sh
 fi
-find $DOCKER_HOME -maxdepth 1 -type d | sed "1d" | xargs chown $DOCKER_USER:$DOCKER_GROUP 2> /dev/null || true
+if [ ! -z "$UPDATE_HOME_WITH_UID" ]; then
+    find $DOCKER_HOME -maxdepth 1 -type d | sed "1d" | xargs chown $DOCKER_USER:$DOCKER_GROUP 2> /dev/null || true
 
-# It is important for $HOME/.ssh to have correct ownership
-chown -R $DOCKER_USER $DOCKER_HOME/.ssh
-
+    # It is important for $HOME/.ssh to have correct ownership
+    chown -R $DOCKER_USER $DOCKER_HOME/.ssh
+fi
 # Initialize config directory
 [ -d $DOCKER_HOME/.config/mozilla ] || cp -r /etc/X11/mozilla $DOCKER_HOME/.config/mozilla
 
