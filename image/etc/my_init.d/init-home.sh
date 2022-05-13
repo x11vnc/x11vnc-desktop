61d21a76fa2784fcdac05739bcbe5221619c9c5e
Xiangmin Jiao <xmjiao@gmail.com>
2022-05-13 19:52:10 -0400

    Change owner for project foler

--- image/etc/my_init.d/init-home.sh
+++ image/etc/my_init.d/init-home.sh
@@ -16,15 +16,10 @@ fi
 # Make sure that all the directories in $HOME are accessible by the user.
 if [ -n "$HOST_UID" -a "$DOCKER_UID" != "$HOST_UID" -o \
     -n "$HOST_GID" -a "$DOCKER_GID" != "$HOST_GID" ]; then
-    find $DOCKER_HOME -maxdepth 1 -type d -not -path "./shared" -not -path "./project" | sed "1d" | \
+    find $DOCKER_HOME -maxdepth 1 -type d -not -path "./shared" | sed "1d" | \
         xargs chown $DOCKER_USER:$DOCKER_GROUP 2> /dev/null || true
 
     # It is important for $HOME/.ssh to have correct ownership
     chown -R $DOCKER_USER:$DOCKER_GROUP $DOCKER_HOME/.ssh
     chown -R $DOCKER_USER:$DOCKER_GROUP $DOCKER_HOME/.config
 fi
-
-# Initialize .config/mozilla directory
-[ -d $DOCKER_HOME/.config/mozilla ] || \
-    cp -r /etc/X11/mozilla $DOCKER_HOME/.config/mozilla && \
-    chown -R $DOCKER_USER:$DOCKER_USER $DOCKER_HOME/.config/mozilla
